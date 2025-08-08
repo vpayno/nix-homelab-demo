888ef961504abb203d17f41dd430540c26f036fa
Victor Payno <vpayno@users.noreply.github.com>
2025-08-08 08:08:27 -0700

    nix(devshell): add default nix develop shell

--- flake.nix
+++ flake.nix
@@ -11,6 +11,11 @@
       url = "github:vpayno/nix-treefmt-conf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nvim-conf = {
+      url = "github:vpayno/neovim-nix-nvf-conf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -19,12 +24,62 @@
       nixpkgs,
       flake-utils,
       treefmt-conf,
+      nvim-conf,
       ...
     }@inputs:
     let
       version = "v0.1.0";
     in
-    flake-utils.lib.eachDefaultSystem (system: {
-      formatter = treefmt-conf.formatter.${system};
-    });
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        # pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+          overlays = [
+          ];
+        };
+
+        commonDevShellPkgs = with pkgs; [
+          bashInteractive
+          coreutils-full
+          git
+          glow
+          home-manager
+          moreutils
+          nixos-anywhere
+          nixos-rebuild
+          npins
+          nvme-cli
+          openssh
+          parted
+          runme
+          tig
+          tmux
+          tree
+        ];
+      in
+      {
+        formatter = treefmt-conf.formatter.${system};
+
+        packages = {
+          nvim-conf = nvim-conf.packages.${system}.default;
+        };
+
+        devShells = {
+          default = pkgs.mkShell {
+            packages = commonDevShellPkgs ++ [ self.packages.${system}.nvim-conf ];
+
+            GREETING = "Starting nix develop shell ${version}...";
+
+            shellHook = ''
+              ${pkgs.lib.getExe pkgs.cowsay} "$GREETING"
+            '';
+          };
+        };
+      }
+    );
 }
